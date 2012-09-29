8a1145b1de40f8ae426acc9c93e68fcdd86beaef
Ian Hill <ianhill@buymeasoda.com>
2012-09-29 22:07:48 +1000

    Enhance logic to allow configuring of counter display modes (always show, only show on focus)

--- characterCount.js
+++ characterCount.js
@@ -2,6 +2,7 @@
 
     $.characterCount = {
         defaults: {
+            alwaysShowCount: false,
             counterClass: 'counter',
             exceededClass: 'exceeded',
             countHTML: '<div></div>'
@@ -10,7 +11,7 @@
             $(fields).trigger('updateCount.characterCount');
         }
     };
-    
+
     function CharacterCount(field, settings) {
         this.field = $(field);
         this.maxLength = settings.maxLength || this.field.attr('maxlength') || this.field.data('maxlength');
@@ -23,19 +24,23 @@
     $.extend(CharacterCount.prototype, {
         initCount: function () {
             this.message = $(this.settings.countHTML);
-            this.message.addClass(this.settings.counterClass).hide().insertAfter(this.field);
-            this.field.on({
-                'input propertychange updateCount.characterCount': $.proxy(this.updateCount, this),
-                'focus': $.proxy(this.showCount, this),
-                'blur': $.proxy(this.hideCount, this)
-            }).removeAttr('maxlength');
+            if (this.settings.alwaysShowCount !== true) {
+                this.message.hide();
+                this.field.on({
+                    'focus': $.proxy(this.showCount, this),
+                    'blur': $.proxy(this.hideCount, this)
+                });
+            }
+            this.message.addClass(this.settings.counterClass).insertAfter(this.field);
+            this.field.on('input propertychange updateCount.characterCount', $.proxy(this.updateCount, this));
+            this.field.removeAttr('maxlength');
             this.updateCount();
         },
         showCount: function () {
             this.message.show();
         },
         hideCount: function () {
-            // this.message.hide();
+            this.message.hide();
         },
         updateCount: function () {
             var count = this.maxLength - this.field.val().length;
@@ -43,7 +48,7 @@
             this.message.toggleClass(this.settings.exceededClass, count < 0);
         }
     });
-    
+
     $.fn.characterCount = function (options) {
         var settings = $.extend({}, $.characterCount.defaults, options || {});
         return this.each(function () {
